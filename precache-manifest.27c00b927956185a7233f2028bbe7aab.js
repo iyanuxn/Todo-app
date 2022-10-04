690746f6c8cdf7f98b26d52aba2225af80aa76bf
iyanuxn <iyanuxn@gmail.com>
2022-10-04 09:49:40 +0100

    Updates

---
+++ precache-manifest.27c00b927956185a7233f2028bbe7aab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a5ea408f57f91749c4e",
+    "url": "/Todo-app/static/css/main.6776d736.chunk.css"
+  },
+  {
+    "revision": "0a5ea408f57f91749c4e",
+    "url": "/Todo-app/static/js/main.0a5ea408.chunk.js"
+  },
+  {
+    "revision": "681e2bcfad3018794b53",
+    "url": "/Todo-app/static/js/1.681e2bcf.chunk.js"
+  },
+  {
+    "revision": "ce0302b16508312457e7",
+    "url": "/Todo-app/static/js/runtime~main.ce0302b1.js"
+  },
+  {
+    "revision": "51207a6b2354833c658b41ff41f73b59",
+    "url": "/Todo-app/static/media/header2.51207a6b.svg"
+  },
+  {
+    "revision": "b454f20c7e82fccd0a831bab8d8b8cd1",
+    "url": "/Todo-app/static/media/error.b454f20c.gif"
+  },
+  {
+    "revision": "a69ce7b334632f7ec7ce626d87800e80",
+    "url": "/Todo-app/static/media/signup.a69ce7b3.svg"
+  },
+  {
+    "revision": "4bf9e97e7f8019624c1a73cda97c945f",
+    "url": "/Todo-app/static/media/login.4bf9e97e.svg"
+  },
+  {
+    "revision": "7163b9fc784c4cf518b18693fc0aeb41",
+    "url": "/Todo-app/index.html"
+  }
+];
